b431d0fa99f058657c168602946ad1ebcd3154ec
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-07-26 02:40:04 +0530

    Added : Created Components for Icon and Text in every page

---
+++ src/components/ChaiAurCodeIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChaiCodeIcon from '../assets/Chai-Aur-Code-Icon.png'
+
+function ChaiAurCodeIcon() {
+    return (
+        <a href="http://www.chaicode.com">
+            <img src={ChaiCodeIcon} className='h-[133.62px] w-[140px] absolute right-10 bottom-10' />
+        </a>
+    )
+}
+
+export default ChaiAurCodeIcon
---
+++ src/components/ChaiAurCodeText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChaiAurCodeText({ extraClasses }) {
+    return (
+        <div className={`font-bold text-6xl text-center pt-10 ${extraClasses}`}>Chai aur Code</div>
+    )
+}
+
+export default ChaiAurCodeText
