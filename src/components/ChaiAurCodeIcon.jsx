54a7e1a684596d2b8ef018a9b9b44c25006880af
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-07-26 11:49:45 +0530

    Update : Modify the ChaiCode Component

--- src/components/ChaiAurCodeIcon.jsx
+++ src/components/ChaiAurCodeIcon.jsx
@@ -3,10 +3,10 @@ import ChaiCodeIcon from '../assets/Chai-Aur-Code-Icon.png'
 
 function ChaiAurCodeIcon() {
     return (
-        <a href="http://www.chaicode.com">
-            <img src={ChaiCodeIcon} className='h-[133.62px] w-[140px] absolute right-10 bottom-10' />
+        <a href="http://www.chaicode.com" className="fixed right-10 bottom-10">
+            <img src={ChaiCodeIcon} className='h-[133.62px] w-[140px]' alt="Chai Aur Code Icon" />
         </a>
-    )
+    );
 }
 
 export default ChaiAurCodeIcon
